df41a1e22cc00a9db54568841f2c804ace99884f
KseniaSafonova <kseniatsn@gmail.com>
2022-10-29 16:18:44 +0300

    .

--- assets/scripts/hackathon.js
+++ assets/scripts/hackathon.js
@@ -1,11 +1,11 @@
-let form = document.getElementById('sectionPrices__form');
+let form = document.getElementById('sectionServices__form');
 
-        function openForm() {
-            form.style.display = 'block';
-        }
+function openForm() {
+    form.style.display = 'block';
+}
 
-        function closeForm() {
-            form.style.display = 'none';
-        }
+function closeForm() {
+    form.style.display = 'none';
+}
 
 
